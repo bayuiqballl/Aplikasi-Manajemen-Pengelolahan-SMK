9bc3c1ff568e09dbc228e21eaa30435b0c83ebb8
bayuiqballl <bayuiqballl13@gmail.com>
2020-04-24 12:09:19 +0700

    add form siswa

--- src/pages/Siswa.js
+++ src/pages/Siswa.js
@@ -4,7 +4,18 @@ function Siswa() {
   return (
     <div className="container mt-5">
       <h1>Data Siswa SMK </h1>
-      <Form></Form>
+      <Form className="row mt-5">
+        <div className="col">
+          <Form.Control placeholder="Nama" />
+        </div>
+        <div className="col">
+          <Form.Control placeholder="NISN" />
+        </div>
+        <div className="col">
+          <Form.Control placeholder="Kelas" />
+        </div>
+        <button>Add Data</button>
+      </Form>
     </div>
   );
 }
